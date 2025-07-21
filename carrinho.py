b26b2da3b1ebf67be7f4e96e8baa4f6d061780d7
Vitoriamotaasm <luanavitoriadasilvamota@gmail.com>
2025-07-20 22:18:48 -0300

    feat: implementar método de adicionar itens ao carrinho

--- carrinho.py
+++ carrinho.py
@@ -1,8 +1,16 @@
 class Carrinho:
     def __init__(self):
-        self.items = []
+        self.itens = []
         self.total = 0.0
 
-    def adicionarProduto(self, produto, quantidade):
+    def adicionar(self, produto, quantidade):
+        if quantidade > produto.estoque:
+            print(f"Erro: Estoque insuficiente para o produto '{produto.nome}'.")
+            return
         self.itens.append({"produto": produto, "quantidade": quantidade})
         self.total += produto.preco * quantidade
+        print(f"{quantidade}x {produto.nome} adicionado(s) ao carrinho.")
+
+    def limpar(self):
+        self.itens = []
+        self.total = 0.0
